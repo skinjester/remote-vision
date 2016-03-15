519f3eeaae6ee826423ccc48ce1762c8747b052d
Gary Boodhoo <gboodhoo@gmail.com>
2016-03-14 21:38:42 -0700

    better presets

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-capture_size = [1920,1080]
+capture_size = [1280, 720]
 viewport_size = [1920,1080]
 now = 0 # timing reference updated each rem cycle
 counter = 0 # has to do with the hud laout. sort of a hack
@@ -8,35 +8,35 @@ guides = []
 guides.append('gaudi1.jpg')
 guides.append('gaudi2.jpg')
 guides.append('house1.jpg')
-guides.append('house2.jpg')
 guides.append('eagle1.jpg')
 guides.append('tiger.jpg')
 guides.append('cat.jpg')
 guides.append('rabbit2.jpg')
 guides.append('eyeballs.jpg')
-guides.append('manuscriptsm.jpg')
 guides.append('spectra.jpg')
 
 models = {}
 models['path'] = 'E:/Users/Gary/Documents/code/models'
-models['cars'] = ['cars','deploy.prototxt','googlenet_finetune_web_car_iter_10000.caffemodel']
-models['googlenet'] = ['bvlc_googlenet','deploy.prototxt','bvlc_googlenet.caffemodel'],
-models['places'] = ['googlenet_places205','deploy.prototxt','googlelet_places205_train_iter_2400000.caffemodel']
+models['cars'] = ('cars','deploy.prototxt','googlenet_finetune_web_car_iter_10000.caffemodel')
+models['googlenet'] = ('bvlc_googlenet','deploy.prototxt','bvlc_googlenet.caffemodel')
+models['places'] = ('googlenet_places205','deploy.prototxt','places205_train_iter_2400000.caffemodel')
 
 
 layers = [
+	'inception_4b/pool',
+	'inception_4e/output',
+	'inception_4d/3x3',
+	'inception_5a/output',
 	'inception_4d/output',
+	'inception_4c/5x5',
+	'inception_3b/3x3_reduce',
+	'inception_4e/output',
+	'inception_4a/3x3',
+	'inception_3b/5x5',
 	'conv2/norm2',
 	'inception_3a/output',
-	'inception_4b/5x5',
-	'inception_4e/output',
 	'inception_3b/3x3',
-	'inception_4d/3x3',
-	'inception_4a/3x3',
-	'inception_3b/3x3_reduce',
-	'inception_3b/5x5',
 	'inception_3b/5x5_reduce',
-	'inception_4b/3x3',
 ]
 
 
@@ -51,26 +51,51 @@ settings['default'] = {
 	'step_mult':0.02,
 	'duration':11,
 }
+
 settings['default2'] = {
-	'iterations':10,
+	'iterations':20,
+	'step_size':2,
+	'octaves':6,
+	'octave_cutoff':4,
+	'octave_scale':1.4,
+	'iteration_mult':0.5,
+	'step_mult':0,
+	'duration':10,
+}
+
+settings['niceplaces'] = {
+	'iterations':50,
 	'step_size':2,
 	'octaves':6,
 	'octave_cutoff':5,
 	'octave_scale':1.5,
-	'iteration_mult':0.5,
-	'step_mult':0.03,
-	'duration':7,
+	'iteration_mult':0.75,
+	'step_mult':0.0,
+	'duration':10,
 }
-settings['tight'] = {
-	'iterations':100,
+
+settings['niceplaces-fast'] = {
+	'iterations':30,
+	'step_size':1,
+	'octaves':7,
+	'octave_cutoff':5,
+	'octave_scale':1.5,
+	'iteration_mult':0.75,
+	'step_mult':0.05,
+	'duration':5,
+}
+
+settings['mckenna'] = {
+	'iterations':20,
 	'step_size':1.0,
 	'octaves':6,
 	'octave_cutoff':4,
-	'octave_scale':1.2,
+	'octave_scale':1.5,
 	'iteration_mult':0.5,
 	'step_mult':0.01,
-	'duration':45,
+	'duration':7,
 }
+
 settings['fast'] = {
 	'iterations':50,
 	'step_size':1.0,
@@ -111,6 +136,16 @@ settings['fast-e'] = {
 	'step_mult':0.01,
 	'duration':2,
 }
+settings['fast-f'] = {
+	'iterations':80,
+	'step_size':1.0,
+	'octaves':6,
+	'octave_cutoff':4,
+	'octave_scale':1.4,
+	'iteration_mult':0.75,
+	'step_mult':0.01,
+	'duration':5,
+}
 settings['hifi'] = {
 	'iterations':30,
 	'step_size':2.0,
@@ -121,23 +156,25 @@ settings['hifi'] = {
 	'step_mult':0.01,
 	'duration':7,
 }
-settings['hifi-tight'] = {
-	'iterations':30,
-	'step_size':2.0,
-	'octaves':6,
-	'octave_cutoff':6,
-	'octave_scale':1.2,
-	'iteration_mult':0.4,
+settings['hifi2'] = {
+	'iterations':15,
+	'step_size':3.0,
+	'octaves':4,
+	'octave_cutoff':4,
+	'octave_scale':1.4,
+	'iteration_mult':0.5,
 	'step_mult':0.01,
-	'duration':19,
+	'duration':12,
 }
-settings['hifi-tight2'] = {
-	'iterations':30,
-	'step_size':2.0,
+
+
+settings['hifi-broad'] = {
+	'iterations':20,
+	'step_size':3.0,
 	'octaves':6,
-	'octave_cutoff':6,
-	'octave_scale':1.1,
-	'iteration_mult':0.4,
-	'step_mult':0.01,
-	'duration':19,
+	'octave_cutoff':5,
+	'octave_scale':1.4,
+	'iteration_mult':0.25,
+	'step_mult':0.0,
+	'duration':5,
 }
