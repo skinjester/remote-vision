59694ca98c0f8c0e32bb05118a8ecff9569d5290
Gary Boodhoo <gboodhoo@gmail.com>
2017-03-12 15:44:09 -0700

    simple script verifies opencv webcam and keyboard input

--- cameratest1.py
+++ cameratest1.py
@@ -2,6 +2,7 @@ import cv2
 
 #capture from camera at location 0
 cap = cv2.VideoCapture(0)
+
 #set the width and height
 cap.set(3,1920)
 cap.set(4,1080)
@@ -15,10 +16,11 @@ while True:
     #cv2.imshow("thresholded", imgray*thresh2)
 	# operations on the frame come here
 
-    key = cv2.waitKey(10)
+    key = cv2.waitKey(10) & 0xEFFFFF
+    print "keyboard input:{}".format(key)
     if key == 27:
         break
 
 
+cap.release()
 cv2.destroyAllWindows() 
-cv2.VideoCapture(0).release()
