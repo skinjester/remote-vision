19837f19e92f0525652b4c9fa4c135fa2b83ffdd
Gary Boodhoo <gboodhoo@gmail.com>
2017-03-21 21:46:16 -0700

    demo scripts for studying Python threading

---
+++ bin/cam-sizer.py
@@ -0,0 +1,26 @@
+import cv2
+
+#capture from camera at location 0
+cap = cv2.VideoCapture(0)
+
+#set the width and height
+cap.set(3,1920)
+cap.set(4,1080)
+
+
+while True:
+    ret, img = cap.read()
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("input", img)
+
+    #cv2.imshow("thresholded", imgray*thresh2)
+	# operations on the frame come here
+
+    key = cv2.waitKey(10) & 0xEFFFFF
+    print "keyboard input:{}".format(key)
+    if key == 27:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows() 
