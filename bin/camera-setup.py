474e9811383041693b6f3a6bfa154e211008190d
Gary Boodhoo <gboodhoo@gmail.com>
2017-11-08 01:01:52 -0800

    adaptive contrast filtering

--- bin/camera-setup.py
+++ bin/camera-setup.py
@@ -41,25 +41,24 @@ while True:
         ret, img = the_camera.read()
 
         if camera_index == 0:
-            img = cv2.flip(cv2.transpose(img),0)
-            cv2.putText(img,'camera 0',(10,20), font, 0.51, (255,255,0), 1, cv2.LINE_AA)
+            img = cv2.flip(cv2.transpose(img),1)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(2,2))
+            equalized = clahe.apply(gray)
+            img = cv2.cvtColor(equalized, cv2.COLOR_GRAY2BGR)
+            cv2.putText(img,'camera 0',(10,20), font, 0.51, (0,255,0), 1, cv2.LINE_AA)
             cv2.imshow('webcam0', img)
         else:
-            img = cv2.flip(cv2.transpose(img),1)
-            cv2.putText(img,'camera 1',(10,20), font, 0.51, (255,255,0), 1, cv2.LINE_AA)
+            img = cv2.flip(cv2.transpose(img),0)
+            cv2.putText(img,'camera 1',(10,20), font, 0.51, (0,255,0), 1, cv2.LINE_AA)
             cv2.imshow('webcam1', img)
 
+    # adaptive contrast filtering
     #img = cv2.transpose(img)
     #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     #equalized = clahe.apply(gray)
 
-
-    #cv2.imshow('webcam0', img)
-    #cv2.imshow('webcam1', img)
-
-    #cv2.imshow("thresholded", imgray*thresh2)
-    # operations on the frame come here
 
     key = cv2.waitKey(10) & 0xFF
     if key == 27: # ESC
