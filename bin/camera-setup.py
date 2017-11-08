8510432a13f1cd61c03c20a89679c6d9b05f9e78
Gary Boodhoo <gboodhoo@gmail.com>
2017-11-08 00:13:06 -0800

    added calibration and setup for multiple camera setup

---
+++ bin/camera-setup.py
@@ -0,0 +1,72 @@
+import cv2
+
+# max 2304 x 1536
+# 1920 x 1080
+# 1600 x 896
+# 1280 x 720
+# 960 x 720
+# 864 x 480
+# 800 x 600
+# 640 x 480
+# 352 x 288
+# 320 x 240
+# 320 x 180
+
+
+# horizontal = cv2.flip( img, 0 )
+# vertical = cv2.flip( img, 1 )
+# horizontal + vertical = cv2.flip( img, -1 )
+
+
+# create named windows
+cv2.namedWindow('webcam0',cv2.WINDOW_NORMAL)
+cv2.namedWindow('webcam1',cv2.WINDOW_NORMAL)
+
+# collect camera objects
+cap = []
+cap.append(cv2.VideoCapture(0))
+cap.append(cv2.VideoCapture(1))
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+# set the width and height
+for index,the_camera in enumerate(cap):
+    the_camera.set(3,1920)
+    the_camera.set(4,1080)
+
+
+while True:
+    for camera_index,the_camera in enumerate(cap):
+        ret, img = the_camera.read()
+
+        if camera_index == 0:
+            img = cv2.flip(cv2.transpose(img),0)
+            cv2.putText(img,'camera 0',(10,20), font, 0.51, (255,255,0), 1, cv2.LINE_AA)
+            cv2.imshow('webcam0', img)
+        else:
+            img = cv2.flip(cv2.transpose(img),1)
+            cv2.putText(img,'camera 1',(10,20), font, 0.51, (255,255,0), 1, cv2.LINE_AA)
+            cv2.imshow('webcam1', img)
+
+    #img = cv2.transpose(img)
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    #equalized = clahe.apply(gray)
+
+
+    #cv2.imshow('webcam0', img)
+    #cv2.imshow('webcam1', img)
+
+    #cv2.imshow("thresholded", imgray*thresh2)
+    # operations on the frame come here
+
+    key = cv2.waitKey(10) & 0xFF
+    if key == 27: # ESC
+        break
+
+
+cv2.destroyAllWindows() 
+
+for the_camera in cap:
+    the_camera.release()
