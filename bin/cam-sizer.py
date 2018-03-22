3094a820ff2fa74c58c46e34ce8e843ac29e358c
Gary Boodhoo <gboodhoo@gmail.com>
2018-03-21 20:29:55 -0700

    added support for transposing and flipping camera view

--- bin/cam-sizer.py
+++ bin/cam-sizer.py
@@ -1,30 +1,35 @@
 import cv2
 
 #capture from camera at location 0
-cameraID = 1
+cameraID = 0
 cap = cv2.VideoCapture(cameraID)
 
 #set the width and height
-cap.set(3,3840)
-cap.set(4,2160)
+cap.set(3,1920)
+cap.set(4,1080)
 
+def show(img, portrait_alignment=False, flip_h=False, flip_v=False):
+    if portrait_alignment: 
+        img = cv2.transpose(img)
+    if flip_v:
+        img = cv2.flip(img, 0)
+    if flip_h:
+        img = cv2.flip(img, 1)
+
+    cv2.imshow("camera:{}  portrait_alignment:{}  flip_h:{}  flip_v:{}"
+        .format(cameraID, portrait_alignment, flip_h, flip_v),
+        img)
+
+    return img
 
 while True:
-    ret, img2 = cap.read()
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    img = img2
-    # img = img2[0:720, 320:1600] # cropped to 1280 x 720
-    cv2.imshow("input", img)
-
-    #cv2.imshow("thresholded", imgray*thresh2)
-	# operations on the frame come here
+    ret, img = cap.read()
+    img = show(img, portrait_alignment=True, flip_h=False, flip_v=True)
 
     key = cv2.waitKey(10) & 0xEFFFFF
     print "camera: {} keyboard input: {} img shape: {}".format(cameraID, key, img.shape)
     if key == 27:
         break
-
 
 cap.release()
 cv2.destroyAllWindows() 
