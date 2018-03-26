0d9d5f7bfe7316da32951bcbeb3f91e66b557329
Gary Boodhoo <gboodhoo@gmail.com>
2018-03-25 20:30:56 -0700

    updated performance settings for runtime

--- deepdreamvisionquest/rem.py
+++ deepdreamvisionquest/rem.py
@@ -1072,16 +1072,16 @@ Device = [0,1] # debug
 w = data.capture_w  # capture width
 h = data.capture_h # capture height
 
-Camera.append(WebcamVideoStream(Device[1], w, h, portrait_alignment=True, log=update_HUD_log, flip_h=True, flip_v=True, gamma=0.7, floor=4000, threshold_filter=16).start())
+Camera.append(WebcamVideoStream(Device[0], w, h, portrait_alignment=True, log=update_HUD_log, flip_h=True, flip_v=True, gamma=0.7, floor=4000, threshold_filter=16).start())
 Webcam = Cameras(source=Camera, current=Device[0])
 
 # --- DISPLAY ---
 Display = Display(width=w, height=h, camera=Webcam.get())
-Viewport = Viewport('deepdreamvisionquest','silent', listener) # no screenshots if username 'silent'
+Viewport = Viewport('deepdreamvisionquest','LAST Festival 2018-2', listener) # no screenshots if username 'silent'
 Composer = Composer()
 
 # --- PERFORMANCE SETTINGS AND rFX ---c
-Model = Model(program_duration=-1) # seconds each program will run, -1 is manual
+Model = Model(program_duration=60) # seconds each program will run, -1 is manual
 Model.set_program(0) # start with program[0]``
 FX = FX()
 
